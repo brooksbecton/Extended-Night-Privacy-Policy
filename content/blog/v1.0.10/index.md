c767415cf9e77524231e85cb1cea7769664c049e
Brooks Becton <brooksbecton1@gmail.com>
2021-03-23 18:05:53 -0400

    Start Gatsby Site

---
+++ content/blog/v1.0.10/index.md
@@ -0,0 +1,24 @@
+---
+title: "Version 1.0.10"
+date: "2021-03-23T22:12:03.284Z"
+description: "Colored maps, Custom Trophies, Trophy Tabs, Change Log Screen"
+---
+
+## New Additions
+- Colored maps for Timberwolf Mountain, Ash Canyon, and Hushed River Valley
+- Custom Trophies
+- Trophy Tabs  to hide completed trophies
+- New Change Log Screen
+- Other various improvements
+
+## What's Next? 
+1. Transition Zone Maps (Ravine, Crumbling Highway, etc.)
+1. Remove Ads Option
+1. Items
+1. Journal
+
+## Notes
+
+There has been a few questions about adding layers to the maps, such as, only showing wolfs, being able to draw on the map, etc. These are great ideas, but would not be possible with how the maps are implemented currently. I'll continue to watch the tech powering the app to see if this can be done in the future.
+
+This has been such an exciting project for me and I am so excited to have support from the community. Thank you all.
---
+++ src/components/layout.js
@@ -0,0 +1,50 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import "@fontsource/satisfy"
+import { AiFillAndroid, AiFillApple } from "react-icons/ai"
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
+  let header
+
+  if (isRootPath) {
+    header = (
+      <h1 className="main-heading">
+        <Link to="/">{title}</Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <Link className="header-link-home" to="/">
+        {title}
+      </Link>
+    )
+  }
+
+  return (
+    <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <header
+        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+        className="global-header"
+      >
+        <span style={{ flex: 1 }}>{header}</span>
+
+        <span>
+          <a href="https://play.google.com/store/apps/details?id=com.broabect.greatnorthguide">
+            <AiFillAndroid size={48} />
+          </a>
+          <a href="https://apps.apple.com/us/app/extended-night-companion-app/id1554875268">
+            <AiFillApple size={48} />
+          </a>
+        </span>
+      </header>
+      <main>{children}</main>
+      {/* <footer style={{display: "flex", alignItems: "center", flexDirection: "row", justifyContent: "flex-end"}}>
+      
+      </footer> */}
+    </div>
+  )
+}
+
+export default Layout
